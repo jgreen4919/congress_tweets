02863361dc50ac78c92cbb2cdca70b81111b5821
jgreen4919 <jon.green.440@gmail.com>
2019-06-19 09:45:59 -0400

    Update congress_tweets.R

--- congress_tweets.R
+++ congress_tweets.R
@@ -28,20 +28,30 @@ remaining <- function(q, default = 15){
 }
 
 # get follows
+# set next cursor
 nc <- "-1"
 congress.follows <- bind_rows(lapply(members$screen_name, function(x){
+  
+  #print progress to give you an idea of how much is left
   print(which(members$screen_name == x))
   
+  # get how many pages of follows need to be scraped (only a handful will be more than one)
   rounds <- ceiling(members$friends_count[members$screen_name == x] / 5000)
   
+  # set next cursor
   nc <<- "-1"
   
+  # get friends
   friends <- lapply(1:rounds, function(y){
+    # get number of get_friends() calls left before rate limit
     limit <- remaining(q = "friends/ids")
+    
+    # if at the limit, wait 15 minutes to refresh (this is the slow part)
     if(limit == 0){
       Sys.sleep(60 * 15 + 10)
     }
     
+    # get friends on that page
     page <- rtweet::get_friends(x, page = nc)
     
     nc <<- rtweet::next_cursor(page)
@@ -49,6 +59,7 @@ congress.follows <- bind_rows(lapply(members$screen_name, function(x){
     return(page)
   })
   
+  # stack pages on top of each other if necessary
   fdat <- unique(bind_rows(friends))
   
   return(fdat)
